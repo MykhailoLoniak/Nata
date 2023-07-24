9603333058e41fe89e23eba338fcbaf89e2850b3
MykhailoLoniak <139628544+MykhailoLoniak@users.noreply.github.com>
2023-07-24 11:59:39 +0200

    Create Header.js

---
+++ New folder/src/components/Header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div className='heder'>
+      <img className='logo' src='\logo.png' alt='' />
+      <h1>Natali Prichina</h1>
+    </div>
+  );
+}
+
+export default Header;
