098c71ba35c5db7300ae232888ce074724462e7f
MykhailoLoniak <139628544+MykhailoLoniak@users.noreply.github.com>
2023-07-24 11:55:38 +0200

    Create App.js

---
+++ New folder/src/App.js
@@ -0,0 +1,14 @@
+import Posts from './components/Posts';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
